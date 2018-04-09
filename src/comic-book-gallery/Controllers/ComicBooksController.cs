a57daee0226b9da7655fd8398dbe289f5bed6dcc
mahfoudhebkar <dah.ebkre@gmail.com>
2018-04-09 12:23:27 -0400

    Added Detail action method

---
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comic_book_gallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
+    }
+}
