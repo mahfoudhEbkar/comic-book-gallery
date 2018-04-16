c0c231f68e2129636c75130d3515f2b0159b038e
mahfoudhebkar <dah.ebkre@gmail.com>
2018-04-16 13:28:04 -0400

    added comic book repository

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using comic_book_gallery.Data;
 
 namespace comic_book_gallery.Controllers
 {
@@ -12,29 +13,9 @@ namespace comic_book_gallery.Controllers
 
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                Favorite = true,
-                DescriptionHtml = "<p>" +
-                        "Final issue! Witness the final hours of Doctor Octopus' life and his one, " +
-                        "last, great act of revenge! Even if Spider-Man survives... " +
-                        "<strong>will Peter Parker?</strong></p>",
-
-                Artists = new Artist[]
-                {
-                    new Artist() { Role= "script", Name = "dan slott"},
-                    new Artist() { Role= "pencils", Name = "humberto ramos"},
-                    new Artist() { Role= "inks", Name = "victor olazaba"},
-                    new Artist() { Role= "colors", Name = "edgar delgado"},
-                    new Artist() { Role= "letters", Name = "chris eliopoulos"},
-
-                }
-
-            };
-
-
+            var comicBookRepository = new ComicBookRepository();
+            var comicBook = comicBookRepository.GetComicBook(1);
+            
             return View(comicBook);
 
         }
